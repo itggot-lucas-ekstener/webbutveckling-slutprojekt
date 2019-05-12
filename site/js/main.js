373709e200298bc8eb56c9505663d6daffc41209
itggot-lucas-ekstener <43437993+itggot-lucas-ekstener@users.noreply.github.com>
2019-05-12 21:39:38 +0200

    Header försvinner på scroll. Buggfix så att den inte försvinner när menyn är uppe. Börjat på resten av sidorna

--- site/js/main.js
+++ site/js/main.js
@@ -1,12 +1,35 @@
 let menu = document.querySelector("menu")
 let menu_icon = document.querySelector(".menu_icon")
+let menu_icon_alt = document.getElementById('nav-icon3');
+let dark = document.querySelector(".dark")
+dark.addEventListener('click', menuToggle)
+let header = document.querySelector(".header")
+
 
 function menuToggle(event){
     menu.classList.toggle("hidden")
+    dark.classList.toggle("hidden_dark")
+    header.classList.toggle("header")
 }
-let menu_icon_alt = document.getElementById('nav-icon3');
+
 function iconTransform(event){
     menu_icon_alt.classList.toggle('open')
 }
 
-
+var prevScrollpos = window.pageYOffset;
+let hideClass = "header_hide"
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (menu.classList.contains('hidden')){
+        if (prevScrollpos > currentScrollPos) {
+            if (header.classList.contains(hideClass)){
+                    header.classList.remove(hideClass)
+            }
+        } else {
+            if (!header.classList.contains(hideClass)){
+                header.classList.add(hideClass)
+            }
+        }
+    } else {}
+prevScrollpos = currentScrollPos;
+}
