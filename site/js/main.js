ffb05f46106fcc76793516d7e1da02208915f4cb
itggot-lucas-ekstener <43437993+itggot-lucas-ekstener@users.noreply.github.com>
2019-06-04 00:23:01 +0200

    fonter, divitis-fix, lite mer avancerad layout på text

--- site/js/main.js
+++ site/js/main.js
@@ -1,26 +1,26 @@
-let menu = document.querySelector("menu")
-let menu_icon = document.querySelector(".menu_icon")
-let menu_icon_alt = document.getElementById('nav-icon3');
+let nav = document.querySelector("nav")
+let nav_icon = document.querySelector(".nav_icon")
+let nav_icon_alt = document.querySelector('.nav-icon3');
 let dark = document.querySelector(".dark")
-dark.addEventListener('click', menuToggle)
+dark.addEventListener('click', navToggle)
 dark.addEventListener('click', iconTransform)
 let header = document.querySelector(".header")
 
 
-function menuToggle(event){
-    menu.classList.toggle("hidden")
+function navToggle(event){
+    nav.classList.toggle("hidden")
     dark.classList.toggle("hidden_dark")
 }
 
 function iconTransform(event){
-    menu_icon_alt.classList.toggle('open')
+    nav_icon_alt.classList.toggle('open')
 }
 
 var prevScrollpos = window.pageYOffset;
 let hideClass = "header_hide"
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (menu.classList.contains('hidden')){
+    if (nav.classList.contains('hidden')){
         if (prevScrollpos > currentScrollPos) {
             if (header.classList.contains(hideClass)){
                     header.classList.remove(hideClass)
